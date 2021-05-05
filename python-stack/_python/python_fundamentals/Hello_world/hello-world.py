8cab688a27d60c1f58e600e2cda9c98976d23f27
karmel yacoub <johndoe@example.com>
2021-05-05 14:27:47 +0300

    test

--- python-stack/_python/python_fundamentals/Hello_world/hello-world.py
+++ python-stack/_python/python_fundamentals/Hello_world/hello-world.py
@@ -13,3 +13,5 @@ fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} {}.format(fave_food1,fave_food2)") # with .format()
 print( f"I love to eat {fave_food1} {fave_food2}") # with an f string
+
+#Test
